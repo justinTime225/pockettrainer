39fb2551d4e9c8f971c28907aec7ba41087e67a9
Justin <justinminh225@gmail.com>
2016-07-16 21:00:16 -0700

    Create schema and insertions for chest table

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,25 +1,8 @@
-DROP DATABASE IF EXISTS puppies;
-CREATE DATABASE puppies;
-
-\c puppies;
-
-CREATE TABLE pups (
-  ID SERIAL PRIMARY KEY,
-  name VARCHAR,
-  breed_name VARCHAR,
-  age INTEGER,
-  sex VARCHAR
-);
-
-INSERT INTO pups (name, breed_name, age, sex)
-  VALUES ('Tyler', 'Retrieved', 3, 'M');
-
-
-
 DROP DATABASE IF EXISTS training;
 CREATE DATABASE training;
 
 \c training;
+
 
 CREATE TABLE chest (
   ID SERIAL PRIMARY KEY,
@@ -32,12 +15,37 @@ CREATE TABLE chest (
   heavy_rep_max INTEGER,
   break_under_min INTEGER,
   core INTEGER,
-  superset INTEGER
-
+  superset_type VARCHAR
+  superset_ID INTEGER,
+  build_type VARCHAR
 );
 
 INSERT INTO chest (name, set_min, set_max, light_rep_min, light_rep_max, 
-          heavy_rep_min, heavy_rep_max, break_under_min, core, superset)
-  VALUES ("Incline Press", 4, 5, 8, 12, 4, 6, 1, 1, 2);
+heavy_rep_min, heavy_rep_max, break_under_min, core, superset_type, superset_ID, build_type)
+  VALUES ("Incline Press", 4, 5, 8, 12, 4, 6, 1, 1, 'Bicep', 2, "Bulk");
+
+INSERT INTO chest (name, set_min, set_max, light_rep_min, light_rep_max, 
+heavy_rep_min, heavy_rep_max, break_under_min, core, superset_type, superset_ID, build_type)
+  VALUES ("Flat Press", 4, 5, 8, 12, 4, 6, 1, 1, 'Bicep', 2, "Bulk");
+
+INSERT INTO chest (name, set_min, set_max, light_rep_min, light_rep_max, 
+heavy_rep_min, heavy_rep_max, break_under_min, core, superset_type, superset_ID, build_type)
+  VALUES ("Decline Press", 4, 5, 8, 12, 4, 8, 1, 1, 'Bicep', 2, "Bulk");
+
+INSERT INTO chest (name, set_min, set_max, light_rep_min, light_rep_max, 
+heavy_rep_min, heavy_rep_max, break_under_min, core, superset_type, superset_ID, build_type)
+  VALUES ("Chest Flyes", 3, 4, 15, 20, 5, 10, 1, 0, 'Chest', 5, "Tone");
+
+INSERT INTO chest (name, set_min, set_max, light_rep_min, light_rep_max, 
+heavy_rep_min, heavy_rep_max, break_under_min, core, superset_type, superset_ID, build_type)
+  VALUES ("Dips", 3, 4, 6, 12, 15, 20, 1, 0, 'Chest', 2, "Tone");
+
+
+
+
+
+
+
+
 
 
