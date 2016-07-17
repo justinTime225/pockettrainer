5a4567f1be3e17ae8d689c41a491df9484d0543d
Justin <justinminh225@gmail.com>
2016-07-16 21:32:09 -0700

    Add chest schema and insert all rows for chest table

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -15,30 +15,30 @@ CREATE TABLE chest (
   heavy_rep_max INTEGER,
   break_under_min INTEGER,
   core INTEGER,
-  superset_type VARCHAR
+  superset_type VARCHAR,
   superset_ID INTEGER,
   build_type VARCHAR
 );
 
 INSERT INTO chest (name, set_min, set_max, light_rep_min, light_rep_max, 
 heavy_rep_min, heavy_rep_max, break_under_min, core, superset_type, superset_ID, build_type)
-  VALUES ("Incline Press", 4, 5, 8, 12, 4, 6, 1, 1, 'Bicep', 2, "Bulk");
+  VALUES ('InclinePress', 4, 5, 8, 12, 4, 6, 1, 1, 'Bicep', 2, 'Bulk');
 
 INSERT INTO chest (name, set_min, set_max, light_rep_min, light_rep_max, 
 heavy_rep_min, heavy_rep_max, break_under_min, core, superset_type, superset_ID, build_type)
-  VALUES ("Flat Press", 4, 5, 8, 12, 4, 6, 1, 1, 'Bicep', 2, "Bulk");
+  VALUES ('FlatPress', 4, 5, 8, 12, 4, 6, 1, 1, 'Bicep', 2, 'Bulk');
 
 INSERT INTO chest (name, set_min, set_max, light_rep_min, light_rep_max, 
 heavy_rep_min, heavy_rep_max, break_under_min, core, superset_type, superset_ID, build_type)
-  VALUES ("Decline Press", 4, 5, 8, 12, 4, 8, 1, 1, 'Bicep', 2, "Bulk");
+  VALUES ('DeclinePress', 4, 5, 8, 12, 4, 8, 1, 1, 'Bicep', 2, 'Bulk');
 
 INSERT INTO chest (name, set_min, set_max, light_rep_min, light_rep_max, 
 heavy_rep_min, heavy_rep_max, break_under_min, core, superset_type, superset_ID, build_type)
-  VALUES ("Chest Flyes", 3, 4, 15, 20, 5, 10, 1, 0, 'Chest', 5, "Tone");
+  VALUES ('ChestFlyes', 3, 4, 15, 20, 5, 10, 1, 0, 'Chest', 5, 'Tone');
 
 INSERT INTO chest (name, set_min, set_max, light_rep_min, light_rep_max, 
 heavy_rep_min, heavy_rep_max, break_under_min, core, superset_type, superset_ID, build_type)
-  VALUES ("Dips", 3, 4, 6, 12, 15, 20, 1, 0, 'Chest', 2, "Tone");
+  VALUES ('Dips', 3, 4, 6, 12, 15, 20, 1, 0, 'Chest', 2, 'Tone');
 
 
 
