0ec8059998ffc0fa73d9d3314b8fe600a7b3c5ab
Justin <justinminh225@gmail.com>
2016-07-16 21:32:47 -0700

    Add chest query function

--- server/db/queries.js
+++ server/db/queries.js
@@ -6,16 +6,27 @@ const options = {
 };
 
 const pgp = require('pg-promise')(options);
-const connectionString = 'postgres://localhost:5432/puppies';
+const connectionString = 'postgres://localhost:5432/training';
 const db = pgp(connectionString);
 
+
 // add query functions
+function getChestExercises(req, res, next) {
+  db.any('select * from chest')
+    .then(function resolve(data) {
+      res.status(200)
+        .json({
+          status: 'success',
+          data: data,
+          message: 'Retrieved ALL chest exercises'
+        });
+    })
+    .catch(function error(err) {
+      return next(err);
+    });
+}
 
-// module.exports = {
-//   getAllPuppies: getAllPuppies,
-//   getSinglePuppy: getSinglePuppy,
-//   createPuppy: createPuppy,
-//   updatePuppy: updatePuppy,
-//   removePuppy: removePuppy
-// };
 
+module.exports = {
+  getChestExercises: getChestExercises
+};
