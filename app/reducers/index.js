74e1f6d5a8ff8236e6f7658666e9d63c182c0d0a
Justin <justinminh225@gmail.com>
2016-07-18 19:09:27 -0700

    Connect basicWorkout reducer function to index.js

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
-
+import basicWorkout from './basicWorkouts.reducer.js';
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  basicWorkout: basicWorkout
 });
 
 export default rootReducer;
